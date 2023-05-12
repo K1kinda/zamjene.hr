7f8bbb95dbef73b26cc38d3662ee76b88272531a
K1kinda <74876252+K1kinda@users.noreply.github.com>
2023-05-12 21:59:51 +0200

    models update

--- website/models.py
+++ website/models.py
@@ -1,5 +1,51 @@
 from . import db
 
-#class Movie(db.Model):
-    #id = db.Column(db.Integer, primary_key=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey('school.id'), nullable=False)
+    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'), nullable=False)
 
+    school = db.relationship('School', backref=db.backref('users', lazy=True))
+    classroom = db.relationship('Classroom', backref=db.backref('users', lazy=True))
+
+
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    login_password = db.Column(db.String(100), nullable=False)
+
+
+class Classroom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey('school.id'), nullable=False)
+
+    school = db.relationship('School', backref=db.backref('classrooms', lazy=True))
+
+
+class Zamjene(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'), nullable=False)
+    # Other fields for zamjene
+
+
+class RasporedSati(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'), nullable=False)
+    # Other fields for rasporedsati
+
+
+class RasporedUcionica(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'), nullable=False)
+    # Other fields for rasporeducionica
+
+
+class Obavjesti(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    classroom_id = db.Column(db.Integer, db.ForeignKey('classroom.id'), nullable=False)
+    # Other fields for obavjesti
