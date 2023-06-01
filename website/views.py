ede0703c8b30d48af653f771eeec844b0d550fb9
mato05 <oosman.matoo@gmail.com>
2023-06-01 21:02:40 +0200

    Napravljen register site, neki mali fixovi

--- website/views.py
+++ website/views.py
@@ -21,3 +21,13 @@ def login():
         return render_template("templates-pc/login.html")
     else:
         return render_template("templates-pc/login.html")
+    
+@views.route('/register')
+def register():
+    user_agent = request.headers.get('User-Agent')
+    if 'Mobile' in user_agent:
+        return render_template("templates-mobile/register_mobile.html")
+    elif 'Windows' in user_agent:
+        return render_template("templates-pc/register.html")
+    else:
+        return render_template("templates-pc/register.html")
