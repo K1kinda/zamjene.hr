60903be9f03b27dd6c0b5632aab6050545605dee
K1kinda <74876252+K1kinda@users.noreply.github.com>
2023-06-24 00:29:16 +0200

    Register/login (needs small updates)
    added a register, login and view profile function
    need to add error messages and fix bugs if any are found

--- website/views.py
+++ website/views.py
@@ -1,18 +1,22 @@
 from flask import Blueprint, render_template, Flask, request, redirect, url_for
 from .models import User
 from . import db
+import time
 
 views = Blueprint('views', __name__)
+
+isLoggedIn=False
+loggedUser = ""
 
 @views.route('/')
 def home():
     user_agent = request.headers.get('User-Agent')
     if 'Mobile' in user_agent:
-        return render_template("templates-mobile/home_mobile.html")
+        return render_template("templates-mobile/home_mobile.html", isLoggedIn=isLoggedIn)
     elif 'Windows' in user_agent:
-        return render_template("templates-pc/home.html")
+        return render_template("templates-pc/home.html", isLoggedIn=isLoggedIn)
     else:
-        return render_template("templates-pc/home.html")
+        return render_template("templates-pc/home.html", isLoggedIn=isLoggedIn)
     
 @views.route('/login')
 def login():
@@ -35,7 +39,8 @@ def register():
         return render_template("templates-pc/register.html")
     
 @views.route('/registersend', methods=['GET', 'POST'])
-def signup():
+def registersend():
+    global isLoggedIn, loggedUser
     if request.method=="POST":
         name = request.form['name']
         surname = request.form['surname']
@@ -44,7 +49,85 @@ def signup():
         schoolID = request.form['schoolID']
         classID = request.form['classID']
 
-        newUser = User(name = name, lastname=surname, email=email, password=password, school_id=schoolID, classroom_id=classID)
-        db.session.add(newUser)
-        db.session.commit()
-        return redirect("/")
+        #provjera jesu li uneseni podatci valjani
+        if len(email)>5:
+            if "@" in email:
+                if "." in email:
+                    pass
+                else:
+                    return redirect("/register")
+            else:
+                return redirect("/register")
+        else:
+            return redirect("/register")
+        has_letter = any(char.isalpha() for char in password)
+        has_number = any(char.isdigit() for char in password)
+        if len(password)>=8:
+            if has_letter and has_number:
+                pass
+            else:
+                return redirect("/register")
+        else:
+            return redirect("/register")
+        if len(schoolID)!=0:
+            try:
+                int(schoolID)
+                pass
+            except ValueError:
+                return redirect("/register")
+        if len(classID)!=0:
+            try:
+                int(classID)
+                pass
+            except ValueError:
+                return redirect("/register")
+        
+
+        #provjera postoji li račun s tim emailom
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            return redirect("/register")
+        else:
+            newUser = User(name = name, lastname=surname, email=email, password=password, school_id=schoolID, classroom_id=classID)
+            db.session.add(newUser)
+            db.session.commit()
+
+            #login novog usera
+            user = User.query.filter_by(email=email).first()
+            isLoggedIn = True
+            loggedUser = user
+            return redirect("/")
+
+    
+@views.route('/loginsend', methods=['GET', 'POST'])
+def loginsend():
+    global isLoggedIn, loggedUser
+    if request.method=="POST":
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password==password:
+                isLoggedIn = True
+                loggedUser = user
+                return redirect("/")
+            else:
+                isLoggedIn = False
+                return redirect("/login")
+        else:
+            isLoggedIn = False
+            return redirect("/login")
+        
+@views.route('/logout')
+def logout():
+    global isLoggedIn
+    isLoggedIn = False
+    return redirect("/")
+
+@views.route('/viewprofile')
+def viewprofile():
+    global loggedUser
+    return render_template("templates-pc/profile.html", user=loggedUser)
+    
+    
